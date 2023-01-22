33a7fcf9b2553dde41af3f355c41aefdda89139b
SaratAngajala <saratangajala@gmail.com>
2023-01-22 16:18:49 +0530

    Hero page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    sassOptions: {},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
